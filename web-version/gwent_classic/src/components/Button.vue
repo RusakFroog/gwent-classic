9452fa0d0a6f64d3f94b7a5850cbf5094b781cd8
wasiliylolita <WasiliyLolita6@gmail.com>
2024-06-02 15:53:53 +0200

    start of making registration

---
+++ web-version/gwent_classic/src/components/Button.vue
@@ -0,0 +1,89 @@
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+
+        active: {
+            type: Boolean,
+            default: false
+        },
+
+        right: {
+            type: Boolean,
+            default: false
+        },
+    },
+
+    computed: {
+        getFloat() {
+            return this.right ? "ml-auto" : "mr-5";
+        },
+
+        isActive() {
+            return this.active ? "active" : "";
+        },
+
+        getStyles() {
+            return [this.getFloat, this.isActive];
+        }
+    }
+};
+</script>
+
+<template>
+    <li :class="getStyles" @click.prevent="onClick">
+        <a>{{ title }}</a>
+    </li>
+</template>
+
+<style scoped>
+li {
+    transition: 0.3s;
+    cursor: pointer;
+    background-image: url('../assets/buttons/frame.svg');
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 320px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.active {
+    background-image: url('../assets/buttons/selected-frame.svg');
+    height: 150px;
+}
+
+.active a {
+    color: black;
+}
+
+li a {
+    display: block;
+    color: #D2B47C;
+    text-decoration: none;
+    text-align: center;
+    font-size: 36px;
+    line-height: 40px;
+    transition: 0.3s ease;
+    user-select: none;
+}
+
+li:hover {
+    transition: 0.3s;
+    background-image: url('../assets/buttons/frame-hover.svg');
+}
+
+.active:hover {
+    background-image: url('../assets/buttons/selected-frame.svg');
+}
+
+li:hover a {
+    transition: 0.3s;
+    color: black;
+}
+</style>
--- web-version/gwent_classic/src/pages/RegistrationPage.vue
+++ web-version/gwent_classic/src/pages/RegistrationPage.vue
@@ -1,32 +1,47 @@
 <script>
+import Button from '../components/Button.vue';
+import CustomInput from '../components/Input.vue';
+
 export default {
-	data() {
-		return {
-			
-		}
-	}
-}
+    data() {
+        return {
+            isActive: false
+        };
+    },
+    components: {
+        Button,
+        CustomInput
+    },
+    methods: {
+        signUp() {
+            this.isActive = !this.isActive; // Toggle active state
+        }
+    }
+};
 </script>
 
 <template>
-	<section class="flex flex-col items-center h-screen w-screen bg-[#242424] bg-cover bg-no-repeat bg-[url('./assets/images/registration/background.svg')]">
-		<div class="text-center text-white mt-20 mb-20">
-			<h3 class="font-witcher-alternative text-9xl">Witcher</h3>
-			<h1 class="font-witcher-alternative mt-6 text-6xl">Gwent classic</h1>
-		</div>
-		<form class="mt-10 p-8 w-[500px] bg-[#6a752d] bg-opacity-80 rounded-lg shadow-lg">
-			<div class="flex flex-col space-y-6">
-				<input class="p-4 text-xl rounded-md bg-[#2c2c2c] text-white placeholder-[#9e9a9a]" type="text" placeholder="LOGIN"/>
-				<input class="p-4 text-xl rounded-md bg-[#2c2c2c] text-white placeholder-[#9e9a9a]" type="password" placeholder="PASSWORD"/>
-				<input class="p-4 text-xl rounded-md bg-[#2c2c2c] text-white placeholder-[#9e9a9a]" type="email" placeholder="EMAIL"/>
-				<button class="mt-6 py-4 bg-[#a73535] text-white text-xl rounded-md hover:bg-[#b22222] focus:outline-none focus:ring-2 focus:ring-[#8b0000]" type="submit">Register</button>
-			</div>
-		</form>
-	</section>
+    <section class="flex flex-col place-items-start h-screen w-screen bg-[#242424] bg-cover bg-no-repeat bg-[url('./assets/images/registration/background.svg')]">
+        <div class="flex items-center flex-col ms-20 h-screen bg-[#0D0D0D] bg-opacity-65 pe-14 ps-14 relative">
+            <h1 class="font-witcher-alternative mt-6 text-3xl text-[#D2B47C]">Create account</h1>
+            <img class="mt-8" src="../assets/images/registration/logo-png.png"/>
+            <div class="flex items-center space-y-6 flex-col mt-8">
+                <CustomInput :placeholder="'LOGIN'" />
+                <CustomInput :placeholder="'EMAIL'" />
+                <CustomInput :placeholder="'PASSWORD'" />
+                <CustomInput :placeholder="'REPEAT PASSWORD'" />
+            </div>
+            <Button class="button" :title="'CONFIRM'" :active="isActive" @click="signUp" />
+        </div>
+        <img class="absolute top-0 translate-x-[630px] w-[80%] " src="../assets/images/registration/gwent-game.png"/>
+    </section>
 </template>
 
 <style scoped>
+.button {
+	margin-top: 30px;
+}
 * :not(h1, h3) {
-	font-family: "Witcher";
+    font-family: "Witcher";
 }
 </style>
