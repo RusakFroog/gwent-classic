b4935770bd5e991487ab4b1b9f062dd246c7e1a0
wasiliylolita <WasiliyLolita6@gmail.com>
2024-06-12 21:08:49 +0200

    fix background before and after slider rendering

--- web-version/gwent_classic/src/components/VolumeSlider.vue
+++ web-version/gwent_classic/src/components/VolumeSlider.vue
@@ -1,37 +1,42 @@
 <script>
-export default {
+  export default {
     data() {
-        return {
-            volume: 0.5
-        };
+      return {
+        volume: 0.5
+      };
     },
     mounted() {
-        const input = document.querySelector(".slider");
-        input.addEventListener("input", function () {
-            const value = (this.value - this.min) / (this.max - this.min) * 100;
-            this.style.background = `linear-gradient(to top, #F1D177 0%, #F1D177 ${value}%, #fff ${value}%, white 100%)`;
-        });
+      this.updateBackground();
+      const input = document.querySelector(".slider");
+      input.addEventListener("input", this.updateBackground);
     },
     watch: {
-        volume() {
-            console.log(this.volume);
-        }
+      volume() {
+        console.log(this.volume);
+      }
+    },
+    methods: {
+      updateBackground() {
+        const input = document.querySelector(".slider");
+        const value = (this.volume - input.min) / (input.max - input.min) * 100;
+        input.style.background = `linear-gradient(to top, #F1D177 0%, #F1D177 ${value}%, #fff ${value}%, white 100%)`;
+      }
     }
-};
-</script>
+  };
+  </script>
 
 <template>
     <div class="slider-container">
-        <input type="range" min="0" max="1" step="0.01" v-model="volume" class="slider" />
+      <input type="range" min="0" max="1" step="0.01" v-model="volume" class="slider" />
     </div>
-</template>
-
-<style scoped>
-.slider-container {
+  </template>
+  
+  <style scoped>
+  .slider-container {
     width: 100%;
-}
-
-.slider {
+  }
+  
+  .slider {
     border-radius: 3px;
     writing-mode: vertical-lr;
     height: 100px;
@@ -43,15 +48,15 @@ export default {
     opacity: 0.7;
     -webkit-transition: 0.2s;
     transition: opacity 0.2s;
-    background-color: hwb(53 60% 36%);
-}
-
-.slider:hover {
+    background: linear-gradient(to top, #F1D177 0%, #F1D177 50%, #fff 50%, white 100%);
+  }
+  
+  .slider:hover {
     opacity: 1;
     color: #F1D177;
-}
-
-.slider::-webkit-slider-thumb {
+  }
+  
+  .slider::-webkit-slider-thumb {
     -webkit-appearance: none;
     appearance: none;
     border-radius: 3px;
@@ -59,13 +64,14 @@ export default {
     height: 25px;
     background: #F1D177;
     cursor: pointer;
-}
-
-.slider::-moz-range-thumb {
+  }
+  
+  .slider::-moz-range-thumb {
     width: 25px;
     height: 25px;
     background: #F1D177;
     cursor: pointer;
     border-radius: 3px;
-}
-</style>
+  }
+  </style>
+  
