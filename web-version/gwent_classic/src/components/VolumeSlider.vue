c8dd024cc0201e66c88e0ae4c8222538017d78c7
wasiliylolita <WasiliyLolita6@gmail.com>
2024-06-13 12:04:24 +0200

    add sotrege and functional of saving volume between sessions

--- web-version/gwent_classic/src/components/VolumeSlider.vue
+++ web-version/gwent_classic/src/components/VolumeSlider.vue
@@ -1,45 +1,62 @@
 <script>
-  export default {
+export default {
     data() {
-      return {
-        volume: 0.5
-      };
+        return {
+            volume: parseFloat(localStorage.getItem('volume'))
+        };
     },
     mounted() {
-      this.updateBackground();
-      const input = document.querySelector(".slider");
-      input.addEventListener("input", this.updateBackground);
+        this.loadVolume();
+        this.updateBackground();
+        const input = document.querySelector(".slider");
+        input.addEventListener("input", this.updateBackground);
     },
     watch: {
-      volume() {
-        console.log(this.volume);
-      }
+        volume(newVal) {
+            console.log(newVal);
+
+            this.saveVolume();
+        }
     },
     methods: {
-      updateBackground() {
-        const input = document.querySelector(".slider");
-        const value = (this.volume - input.min) / (input.max - input.min) * 100;
-        input.style.background = `linear-gradient(to top, #F1D177 0%, #F1D177 ${value}%, #fff ${value}%, white 100%)`;
-      }
+        updateBackground() {
+            const input = document.querySelector(".slider");
+            if (this.volume < 0.01) {
+                input.style.setProperty('--thumb-color', '#FFFFFF');
+            } else {
+                input.style.setProperty('--thumb-color', '#F1D177');
+            }
+            const value = (this.volume - input.min) / (input.max - input.min) * 100;
+            input.style.background = `linear-gradient(to top, #F1D177 0%, #F1D177 ${value}%, #fff ${value}%, white 100%)`;
+        },
+        saveVolume() {
+            localStorage.setItem('volume', this.volume);
+        },
+        loadVolume() {
+            const savedVolume = localStorage.getItem('volume');
+            if (savedVolume !== null) {
+                this.volume = parseFloat(savedVolume);
+            }
+        }
     }
-  };
-  </script>
+};
+</script>
 
 <template>
     <div class="slider-container">
-      <input type="range" min="0" max="1" step="0.01" v-model="volume" class="slider" />
+        <input type="range" min="0" max="1" step="0.01" v-model="volume" class="slider" />
     </div>
-  </template>
-  
-  <style scoped>
-  .slider-container {
+</template>
+
+<style scoped>
+.slider-container {
     width: 100%;
-  }
-  
-  .slider {
+}
+
+.slider {
     border-radius: 3px;
     writing-mode: vertical-lr;
-    height: 100px;
+    height: 60px;
     direction: rtl;
     vertical-align: middle;
     -webkit-appearance: none;
@@ -49,29 +66,28 @@
     -webkit-transition: 0.2s;
     transition: opacity 0.2s;
     background: linear-gradient(to top, #F1D177 0%, #F1D177 50%, #fff 50%, white 100%);
-  }
-  
-  .slider:hover {
+}
+
+.slider:hover {
     opacity: 1;
     color: #F1D177;
-  }
-  
-  .slider::-webkit-slider-thumb {
+}
+
+.slider::-webkit-slider-thumb {
     -webkit-appearance: none;
     appearance: none;
     border-radius: 3px;
-    width: 25px;
-    height: 25px;
-    background: #F1D177;
+    width: 15px;
+    height: 15px;
+    background: var(--thumb-color, #F1D177);
     cursor: pointer;
-  }
-  
-  .slider::-moz-range-thumb {
-    width: 25px;
-    height: 25px;
-    background: #F1D177;
+}
+
+.slider::-moz-range-thumb {
+    width: 15px;
+    height: 15px;
+    background: var(--thumb-color, #F1D177);
     cursor: pointer;
     border-radius: 3px;
-  }
-  </style>
-  
+}
+</style>
--- web-version/gwent_classic/src/main.js
+++ web-version/gwent_classic/src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index.js';
+import store from './store/Store.js';
+
+store.dispatch('loadVolume');
 
 createApp(App)
+    .use(store)
     .use(router)
     .mount('#app')
