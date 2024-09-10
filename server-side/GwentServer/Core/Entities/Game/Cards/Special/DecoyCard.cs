d0946a9366a86951fa897722c66b94bce6623ee9
Rusak21 <sg970465@gmail.com>
2024-09-10 21:48:31 +0200

    through some major misses

--- server-side/GwentServer/Core/Entities/Game/Cards/Special/DecoyCard.cs
+++ server-side/GwentServer/Core/Entities/Game/Cards/Special/DecoyCard.cs
@@ -4,7 +4,7 @@ namespace Core.Entities.Game.Cards.Special;
 
 public class DecoyCard : Card
 {
-    public DecoyCard() : base("Decoy", -1, Fraction.None, [FieldLine.Closer, FieldLine.Ranger, FieldLine.Siege])
+    public DecoyCard() : base("Decoy", -1, Fraction.None, [FieldLine.Closer, FieldLine.Ranger, FieldLine.Siege], CardCategory.Special)
     {
 
     }
