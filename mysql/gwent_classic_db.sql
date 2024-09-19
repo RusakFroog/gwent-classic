3a17692d0034e2b31d49c9424ef27199a923f990
Rusak21 <sg970465@gmail.com>
2024-09-20 00:31:36 +0200

    upd db

--- mysql/gwent_classic_db.sql
+++ mysql/gwent_classic_db.sql
@@ -19,18 +19,52 @@
 CREATE DATABASE IF NOT EXISTS `gwent_classic` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `gwent_classic`;
 
+-- Дамп структуры для таблица gwent_classic.accounts
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `hashed_password` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `decks` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  CONSTRAINT `accounts_chk_1` CHECK (json_valid(`decks`))
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Экспортируемые данные не выделены.
+
 -- Дамп структуры для таблица gwent_classic.cards
 CREATE TABLE IF NOT EXISTS `cards` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `type_id` tinyint NOT NULL,
   `strength` int NOT NULL,
-  `fraction` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `field_lines` longtext COLLATE utf8mb4_general_ci NOT NULL,
-  `name` longtext COLLATE utf8mb4_general_ci NOT NULL,
-  `can_be_taken` tinyint(1) NOT NULL,
-  `horn_boost` tinyint(1) NOT NULL,
-  `weather_immunity` tinyint(1) NOT NULL,
+  `fraction_id` tinyint NOT NULL DEFAULT (0),
+  `field_lines` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `is_hero` tinyint(1) NOT NULL DEFAULT (0),
+  `muster_cards` longtext COLLATE utf8mb4_general_ci NOT NULL,
   `card_category` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `FK_fractions_cards` (`fraction_id`) USING BTREE,
+  KEY `FK_card_types_cards` (`type_id`),
+  CONSTRAINT `FK_fractions_cards` FOREIGN KEY (`fraction_id`) REFERENCES `fractions` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица gwent_classic.card_types
+CREATE TABLE IF NOT EXISTS `card_types` (
+  `id` tinyint NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'None',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица gwent_classic.fractions
+CREATE TABLE IF NOT EXISTS `fractions` (
+  `id` tinyint NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Экспортируемые данные не выделены.
